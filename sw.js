534558251bb09166686eb090d45fea1218075dbd
Auxillia Jerine <114934206+Auxi98@users.noreply.github.com>
2023-10-11 19:23:04 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+
+
+self.addEventListener('install', function(e) {
+    console.log('Service worker is installed', e);
+  });
+  
+//   self.addEventListener('fetch', function() {
+//     // ...
+//   });
